41b2ef5e19cb82a22c377b305054f01551727797
Eliarlan Macedo <eliarlanrodrigues7@gmail.com>
2021-08-30 20:08:17 -0300

    22.19. Processando o template e enviando o e-mail

--- src/main/java/com/eliarlan/edevmoneyapi/mail/Mailer.java
+++ src/main/java/com/eliarlan/edevmoneyapi/mail/Mailer.java
@@ -1,17 +1,18 @@
 package com.eliarlan.edevmoneyapi.mail;
 
-//import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.context.event.ApplicationReadyEvent;
-//import org.springframework.context.event.EventListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
 public class Mailer {
@@ -19,16 +20,22 @@ public class Mailer {
 	@Autowired
 	private JavaMailSender mailSender;
 	
-//	@EventListener
-//	public void teste(ApplicationReadyEvent event) {
-//		this.enviarEmail("remetente",
-//				Arrays.asList("destinatario"),
-//				"Testando",
-//				"Olá, <br/>Teste OK!");
-//		System.out.println("Envio Finalizado...");
-//	}
+	@Autowired
+	private TemplateEngine thymeleaf;
 	
-	
+	public void enviarEmail(String remetente,
+			List<String> destinatarios, String assunto, String template, 
+			Map<String, Object> variaveis) {
+		
+		Context context = new Context(new Locale("pt", "BR"));
+		
+		variaveis.entrySet().forEach(
+				e -> context.setVariable(e.getKey(), e.getValue()));
+		String mensagem = thymeleaf.process(template, context);
+		
+		this.enviarEmail(remetente, destinatarios, assunto, mensagem);
+		
+	}	
 	
 	public void enviarEmail(String remetente,
 			List<String> destinatarios, String assunto, String mensagem) {
@@ -48,3 +55,21 @@ public class Mailer {
 		
 	}
 }
+
+//@Autowired
+//private LancamentoRepository repo;
+//@EventListener
+//public void teste(ApplicationReadyEvent event) {
+//	String template = "mail/aviso_lancamentos_vencidos";
+//	
+//	List<Lancamento> lista = repo.findAll();
+//	
+//	Map<String, Object> variaveis = new HashMap<>();
+//	variaveis.put("lancamentos", lista);
+//	
+//	
+//	this.enviarEmail("remetente",
+//		Arrays.asList("destinatario"),
+//		"Testando", template, variaveis);
+//	System.out.println("Envio Finalizado...");
+//}
