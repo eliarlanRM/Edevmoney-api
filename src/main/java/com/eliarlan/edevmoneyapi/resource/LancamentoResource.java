0b8f62dde6aad70c4412b4e2424d742f349949ac
Eliarlan Macedo <eliarlanrodrigues7@gmail.com>
2021-08-31 12:51:19 -0300

    22.28. Upload de arquivos para API

--- src/main/java/com/eliarlan/edevmoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/eliarlan/edevmoneyapi/resource/LancamentoResource.java
@@ -1,5 +1,8 @@
 package com.eliarlan.edevmoneyapi.resource;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eliarlan.edevmoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.eliarlan.edevmoneyapi.dto.LancamentoEstatisticaDia;
@@ -58,6 +62,18 @@ public class LancamentoResource {
 	
 	@Autowired
 	private MessageSource messageSource;
+	
+	
+	@PostMapping("/anexo")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
+	public String uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
+		OutputStream out = new FileOutputStream(
+				"c://Users/Eliarlan/Desktop/" + LocalDate.now() + "__"+ anexo.getOriginalFilename());
+		out.write(anexo.getBytes());
+		out.close();
+		
+		return "ok!";
+	}
 	
 	@GetMapping("/relatorios/por-pessoa")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
