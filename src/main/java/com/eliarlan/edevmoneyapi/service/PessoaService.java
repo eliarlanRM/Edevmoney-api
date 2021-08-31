d7e0a984c4dde197703bc1106e600547988206df
Eliarlan Macedo <eliarlanrodrigues7@gmail.com>
2021-08-31 11:22:04 -0300

    22.26. Usando a propriedade orphanRemoval

--- src/main/java/com/eliarlan/edevmoneyapi/service/PessoaService.java
+++ src/main/java/com/eliarlan/edevmoneyapi/service/PessoaService.java
@@ -19,9 +19,11 @@ public class PessoaService {
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
 		Pessoa pessoaSalva = bucarPessoaPeloCodigo(codigo);
 		
-		pessoa.getContatos().forEach(c -> c.setPessoa(pessoa));
+		pessoaSalva.getContatos().clear();
+		pessoaSalva.getContatos().addAll(pessoa.getContatos());
+		pessoaSalva.getContatos().forEach(c -> c.setPessoa(pessoaSalva));
 	
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo", "contatos");
 	
 		return this.pessoaRepository.save(pessoaSalva);
 	}
